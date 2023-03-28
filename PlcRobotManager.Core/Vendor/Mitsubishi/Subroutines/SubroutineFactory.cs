9294fc2bf2122664aa2b5541c7cffa69872951d4
madstone290 <madstone290@gmail.com>
2023-03-28 11:20:36 +0900

    feat add 서브루틴 팩토리 추가

---
+++ PlcRobotManager.Core/Vendor/Mitsubishi/Subroutines/SubroutineFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PlcRobotManager.Core.Vendor.Mitsubishi.Subroutines
+{
+    /// <summary>
+    /// 서브루틴을 생성한다
+    /// </summary>
+    public class SubroutineFactory
+    {
+        public ISubroutine Create(SubroutineDetectionType detectionType, string name, string dictKey, int initCount = 1, bool isStart = false, bool isEnd = false)
+        {
+            switch(detectionType)
+            {
+                case SubroutineDetectionType.CycleTime:
+                    return new CycleTimeSubroutine(name, dictKey, initCount);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(detectionType));
+            }
+        }
+    }
+}
