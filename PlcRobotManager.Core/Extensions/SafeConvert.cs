0ff23bc57763d2b670cf0f539e27d8d14aa573dd
madstone290 <madstone290@gmail.com>
2023-04-04 15:09:53 +0900

    feat 시작종료플래그 서브루틴 추가

---
+++ PlcRobotManager.Core/Extensions/SafeConvert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlcRobotManager.Core.Extensions
+{
+    public class SafeConvert
+    {
+        public static bool ToBoolean(object value)
+        {
+            bool result = false;
+            try
+            {
+                result = Convert.ToBoolean(value);
+            }
+            catch { }
+            return result;
+        }
+    }
+}
--- PlcRobotManager.Core/Vendor/Mitsubishi/Subroutines/StartEndFlagSubroutine.cs
+++ PlcRobotManager.Core/Vendor/Mitsubishi/Subroutines/StartEndFlagSubroutine.cs
@@ -8,16 +8,19 @@ using System.Threading.Tasks;
 namespace PlcRobotManager.Core.Vendor.Mitsubishi.Subroutines
 {
     /// <summary>
-    /// 작업상태시작을 알리는 비트값으로 사이클 상태를 판단한다.
-    /// 비트가 1로 변하는 경우 사이클시작, 0으로 변하는 경우 사이클종료로 판단한다.
-    /// 사이클종료에서 시작까지의 간격이 좁은 경우(수백ms) 통신지연으로(수백ms) 인해 정상적인 사이클탐지에 실패할 가능성이 높다.
+    /// 시작비트 및 종료비트를 이용해 사이클 상태를 판단한다.
     /// </summary>
-    public class StartFlagSubroutine : ISubroutine
+    public class StartEndFlagSubroutine : ISubroutine
     {
         /// <summary>
-        /// 사이클 상태 확인을 위한 키
+        /// 시작비트 키
         /// </summary>
-        private readonly string _flagKey;
+        private readonly string _startFlagKey;
+
+        /// <summary>
+        /// 종료비트 키
+        /// </summary>
+        private readonly string _endFlagKey;
 
         /// <summary>
         /// 서브루틴명
@@ -30,57 +33,62 @@ namespace PlcRobotManager.Core.Vendor.Mitsubishi.Subroutines
         private int _cycleCount = 0;
 
         /// <summary>
-        /// 시작 플래그 값
+        /// 시작플래그 값. false -> true이면 사이클시작
         /// </summary>
         private bool _startFlag = false;
+
+        /// <summary>
+        /// 종료플래그 값. true -> false이면 사이클종료
+        /// </summary>
+        private bool _endFlag = false;
 
         /// <summary>
         /// 최초 사이클인지 확인한다. 프로그램을 처음 구동할 때에는 현재 사이클 상태를 정상적으로 판단할 수 없다.
         /// </summary>
         private bool _isFirstCylce = true;
 
-        public StartFlagSubroutine(string name, string flagKey, int initialCount = 1)
+        public StartEndFlagSubroutine(string startFlagKey, string endFlagKey, string name, int initialCount = 1)
         {
-
+            _startFlagKey = startFlagKey;
+            _endFlagKey = endFlagKey;
             _name = name;
-            _flagKey = flagKey;
             _cycleCount = initialCount;
         }
 
         public string Name => _name;
-        public SubroutineDetectionType DetectionType => SubroutineDetectionType.StartFlag;
+        public SubroutineDetectionType DetectionType => SubroutineDetectionType.StartEndFlag;
         public int CycleCount => _cycleCount;
         public event EventHandler<int> CycleStarted;
         public event EventHandler<int> CycleEnded;
 
         public void CheckCycle(IReadOnlyDictionary<string, object> data)
         {
-            if (!data.TryGetValue(_flagKey, out object value))
+            if (!data.TryGetValue(_startFlagKey, out object startValue))
+                return;
+            if (!data.TryGetValue(_endFlagKey, out object endValue))
                 return;
 
-            bool startFlag = SafeConvert.ToBoolean(value);
-
+            bool startFlag = SafeConvert.ToBoolean(startValue);
+            bool endFlag = SafeConvert.ToBoolean(endValue);
             if (_isFirstCylce) // 첫 1회에는 값만 기록하고 사이클을 판단하지 않는다.
             {
                 _isFirstCylce = false;
                 _startFlag = startFlag;
+                _endFlag = endFlag;
                 return;
             }
 
-            if (_startFlag == startFlag)
-                return;
-
-            if (startFlag)
+            if (!_startFlag && startFlag) // 시작
             {
                 CycleStarted?.Invoke(this, _cycleCount);
             }
-            else
+            else if (!_endFlag && endFlag) // 종료
             {
                 CycleEnded?.Invoke(this, _cycleCount);
                 _cycleCount++;
             }
-
             _startFlag = startFlag;
+            _endFlag = endFlag;
         }
     }
 }
